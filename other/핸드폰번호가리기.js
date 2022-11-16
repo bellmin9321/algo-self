c9619722c8a7bbcc87c555ee874ef3122ae5e66e
musk <cpt@cashmallow.com>
2022-11-17 01:29:01 +0900

    ✅ Add: 핸드폰 번호 가리기 풀이

---
+++ other/핸드폰번호가리기.js
@@ -0,0 +1,7 @@
+function solution(phone_number) {
+  return [...phone_number].fill("*", 0, phone_number.length - 4).join("");
+}
+
+function hide_numbers(s) {
+  return s.replace(/\d(?=\d{4})/g, "*");
+}
