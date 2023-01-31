0f192f0365d4d943012f0acf97bb8cc5cf7cff18
JONGMIN KIM <bellmin9321@gmail.com>
2023-01-31 13:58:32 +0900

    ⭕️ Solved: 푼 문제(Date 객체 이용)

---
+++ src/2016년.js
@@ -0,0 +1,10 @@
+// 내 풀이
+function solution(a, b) {
+  return ["SUN","MON","TUE","WED","THU","FRI","SAT"][new Date(`2016, ${a}, ${b}`).getDay()];
+}
+
+// 다른 사람 풀이
+function solution(a, b) {
+  var date = new Date(2016, (a - 1), b);
+    return date.toString().slice(0, 3).toUpperCase();
+}
