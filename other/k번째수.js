6bad4bd7befa043e2aa8b5d2e966c80772ec432b
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-01 16:13:26 +0900

    ⭕️ Solved: 푼 문제

---
+++ other/k번째수.js
@@ -0,0 +1,14 @@
+// array destucturing 풀이
+
+function solution(array, commands) {
+  return commands.map(command => {
+    const [sPosition, ePosition, position] = command;
+    const newArray = array
+      .filter(
+        (value, fIndex) => fIndex >= sPosition - 1 && fIndex <= ePosition - 1,
+      )
+      .sort((a, b) => a - b);
+
+    return newArray[position - 1];
+  });
+}
