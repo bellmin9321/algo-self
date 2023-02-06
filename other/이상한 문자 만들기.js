7f869c1835255760ef4fa1b62ad5d862ff53f945
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-06 16:45:30 +0900

    ⭕️ Solved: 푼 문제

--- other/이상한 문자 만들기.js
+++ other/이상한 문자 만들기.js
@@ -1,5 +1,9 @@
 function solution(s) {
-  return s.toLowerCase().split(' ').map((v, i) => [...v].map((w, j) => j % 2 === 0 ? w.toUpperCase() : w).join('')).join(' ');
+  return s
+    .toLowerCase()
+    .split(' ')
+    .map((v, i) =>
+      [...v].map((w, j) => (j % 2 === 0 ? w.toUpperCase() : w)).join(''),
+    )
+    .join(' ');
 }
-
-
