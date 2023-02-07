48e9727528d79409036dcb80b828ae2a05d2ee12
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-07 18:34:00 +0900

    ⭕️ Solved: 푼 문제

--- other/문자열내p와y의개수.js
+++ other/문자열내p와y의개수.js
@@ -1,6 +1,6 @@
 function solution(s) {
   return (
-    s.toLowerCase().split("p").length === s.toLowerCase().split("y").length
+    s.toLowerCase().split('p').length === s.toLowerCase().split('y').length
   );
 }
 
@@ -14,8 +14,8 @@ function numPY(s) {
   let num = 0;
 
   for (let i = 0; i < s.length; i++) {
-    if (s[i] === "P") num++;
-    if (s[i] === "Y") num--;
+    if (s[i] === 'P') num++;
+    if (s[i] === 'Y') num--;
   }
   result = num === 0;
 
@@ -24,8 +24,8 @@ function numPY(s) {
 
 function solution(s) {
   return [...s.toLowerCase()].reduce((acc, cur) => {
-    if (cur === "p") return acc + 1;
-    else if (cur === "y") return acc - 1;
+    if (cur === 'p') return acc + 1;
+    else if (cur === 'y') return acc - 1;
     return acc;
   }, 0)
     ? false
