2a59d57ab647ba01a39e03454f85e04d89c26c60
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-07 18:34:04 +0900

    ⭕️ Solved: 푼 문제

--- src/2016년.js
+++ src/2016년.js
@@ -1,10 +1,12 @@
 // 내 풀이
 function solution(a, b) {
-  return ["SUN","MON","TUE","WED","THU","FRI","SAT"][new Date(`2016, ${a}, ${b}`).getDay()];
+  return ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'][
+    new Date(`2016, ${a}, ${b}`).getDay()
+  ];
 }
 
 // 다른 사람 풀이
 function solution(a, b) {
-  var date = new Date(2016, (a - 1), b);
-    return date.toString().slice(0, 3).toUpperCase();
+  var date = new Date(2016, a - 1, b);
+  return date.toString().slice(0, 3).toUpperCase();
 }
