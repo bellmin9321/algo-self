d6939fce0ac995fd512b8853551860d9981bac44
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-17 21:06:53 +0900

    ⭕️ Solved: 재풀이 통과

---
+++ src/시저암호.js
@@ -0,0 +1,22 @@
+// my solution
+function solution(s, n) {
+  let result = '';
+  
+  for (let i = 0; i < s.length; i++) {
+      if (s[i] === ' ') {
+          result += ' ';
+      } else {
+          let codeAt = s[i].charCodeAt() + n;
+          
+          if (s[i] === s[i].toLowerCase()) {
+              if (codeAt > 122) codeAt -= 26;
+          } else {
+              if (codeAt > 90) codeAt -= 26;
+          }
+          
+          result += String.fromCharCode(codeAt)
+      }
+  }어
+  
+  return result;
+}
