662f376090a9164ab8bbd885d5f24e347a19c896
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-18 21:56:04 +0900

    ❌ NotSolved

--- src/짝지어 제거하기.js
+++ src/짝지어 제거하기.js
@@ -1,3 +1,18 @@
+// my solution: 효율성 테스트 실패 ❌
+function solution(s) {
+  const arr = [...s];
+  
+  for (let i = 1; i < arr.length; i++) {
+      if (arr[i - 1] === arr[i]) {
+          arr.splice(i - 1, 2);
+          i = 0;
+      }
+  }
+  
+  return arr.length ? 0 : 1
+}
+
+// 옛날 풀이
 function solution(s) {
   let i = 0;
 
@@ -13,3 +28,4 @@ function solution(s) {
 
   return 1;
 }
+
