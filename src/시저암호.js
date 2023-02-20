82ab2cbc28f8527a8c96d8bf1a739bd3c9bb57c7
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-20 22:05:48 +0900

    ⭕️ Solved

--- src/시저암호.js
+++ src/시저암호.js
@@ -1,22 +1,23 @@
 // my solution
 function solution(s, n) {
   let result = '';
-  
+
   for (let i = 0; i < s.length; i++) {
-      if (s[i] === ' ') {
-          result += ' ';
+    if (s[i] === ' ') {
+      result += ' ';
+    } else {
+      let codeAt = s[i].charCodeAt() + n;
+
+      if (s[i] === s[i].toLowerCase()) {
+        if (codeAt > 122) codeAt -= 26;
       } else {
-          let codeAt = s[i].charCodeAt() + n;
-          
-          if (s[i] === s[i].toLowerCase()) {
-              if (codeAt > 122) codeAt -= 26;
-          } else {
-              if (codeAt > 90) codeAt -= 26;
-          }
-          
-          result += String.fromCharCode(codeAt)
+        if (codeAt > 90) codeAt -= 26;
       }
-  }어
-  
+
+      result += String.fromCharCode(codeAt);
+    }
+  }
+  어;
+
   return result;
 }
