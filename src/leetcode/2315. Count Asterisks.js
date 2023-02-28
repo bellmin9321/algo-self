17e7646b56acbede5775b6e70a5a507a4a7bb0c9
JONGMIN KIM <bellmin9321@gmail.com>
2023-02-28 23:42:11 +0900

    ⭕️ Solved

---
+++ src/leetcode/2315. Count Asterisks.js
@@ -0,0 +1,14 @@
+// my solution
+var countAsterisks = function(s) {
+  return s.split('|').filter((v, i) => i % 2 === 0).join('').split('*').length - 1;
+};
+
+// best
+var countAsterisks = function(s) {
+  let green=true, count=0;
+  for(let i=0; i<s.length; i++){
+      if(green && s[i]=="*"){count++};
+      if(s[i]=="|"){green=!green};
+  }
+  return count;
+};
