78a948f6182b7d1925327b82acc607b0d063bb1d
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-05 16:03:23 +0900

    ⭕️ Solved

--- src/leetcode/2574. Left and Right Sum Differences.js
+++ src/leetcode/2574. Left and Right Sum Differences.js
@@ -1,12 +1,14 @@
 // my solution
-var leftRigthDifference = function(nums) {
-    const leftSum = [0];
-    const rightSum = [0];
+var leftRigthDifference = function (nums) {
+  const leftSum = [0];
+  const rightSum = [0];
 
-    for (let i = 0; i < nums.length - 1; i++) {
-      leftSum.push(leftSum[i] + nums[i]);
-      rightSum.unshift(rightSum[rightSum.length - 1 - i] + nums[nums.length - 1 - i]);
-    }
+  for (let i = 0; i < nums.length - 1; i++) {
+    leftSum.push(leftSum[i] + nums[i]);
+    rightSum.unshift(
+      rightSum[rightSum.length - 1 - i] + nums[nums.length - 1 - i],
+    );
+  }
 
-    return leftSum.map((v, i) => Math.abs(v - rightSum[i]));
+  return leftSum.map((v, i) => Math.abs(v - rightSum[i]));
 };
