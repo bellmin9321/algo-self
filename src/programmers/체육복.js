e8298ccd449a95205b7eb91c04c1f5c209c8f279
JONGMIN KIM <bellmin9321@gmail.com>
2023-03-27 18:21:44 +0900

    ⭕️ Solved

---
+++ src/programmers/체육복.js
@@ -0,0 +1,57 @@
+// my solution
+function solution(n, lost, reserve) {
+  let rf = reserve.filter(v => !lost.includes(v)).sort((a, b) => a - b);
+  let lf = lost.filter(v => !reserve.includes(v)).sort((a, b) => a - b);
+  let save = 0;
+
+  for (let i = 0; i < lf.length; i++) {
+    for (let j = 0; j < rf.length; j++) {
+      if (Math.abs(lf[i] - rf[j]) === 1) {
+        save++;
+        rf.splice(j, 1);
+        break;
+      }
+    }
+  }
+
+  return n - (lf.length - save);
+}
+
+// best
+function solution(n, lost, reserve) {
+  return (
+    n -
+    lost.filter(a => {
+      const b = reserve.find(r => Math.abs(r - a) <= 1);
+      if (!b) return true;
+      reserve = reserve.filter(r => r !== b);
+    }).length
+  );
+}
+
+// 2nd
+function solution(n, lost, reserve) {
+  const students = {};
+  let answer = 0;
+  for (let i = 1; i <= n; i++) {
+    students[i] = 1;
+  }
+  lost.forEach(number => (students[number] -= 1));
+  reserve.forEach(number => (students[number] += 1));
+
+  for (let i = 1; i <= n; i++) {
+    if (students[i] === 2 && students[i - 1] === 0) {
+      students[i - 1]++;
+      students[i]--;
+    } else if (students[i] === 2 && students[i + 1] === 0) {
+      students[i + 1]++;
+      students[i]--;
+    }
+  }
+  for (let key in students) {
+    if (students[key] >= 1) {
+      answer++;
+    }
+  }
+  return answer;
+}
