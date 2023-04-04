64473dd8d06eedd49f094144cf86fb7d94417d8e
JONGMIN KIM <bellmin9321@gmail.com>
2023-04-04 22:14:40 +0900

    ⭕️ Solved

---
+++ src/programmers/카드 뭉치.js
@@ -0,0 +1,69 @@
+// my solution
+function solution(cards1, cards2, goal) {
+  for (let i = 0; i < goal.length; i++) {
+    if (goal[i] === cards1[i]) {
+      goal.splice(i, 1);
+      cards1.splice(i, 1);
+      i--;
+      continue;
+    }
+
+    if (goal[i] === cards2[i]) {
+      goal.splice(i, 1);
+      cards2.splice(i, 1);
+      i--;
+      continue;
+    }
+  }
+
+  return goal.length ? 'No' : 'Yes';
+}
+// my solution 2
+function solution(cards1, cards2, goal) {
+  for (let i = 0; i < goal.length; i++) {
+    if (goal[i] === cards1[0]) {
+      cards1.shift();
+      continue;
+    }
+
+    if (goal[i] === cards2[0]) {
+      cards2.shift();
+      continue;
+    }
+
+    return 'No';
+  }
+
+  return 'Yes';
+}
+
+// best
+function solution(cards1, cards2, goal) {
+  for (const s of goal) {
+    if (cards1[0] == s) {
+      cards1.shift();
+    } else if (cards2[0] == s) {
+      cards2.shift();
+    } else {
+      return 'No';
+    }
+  }
+
+  return 'Yes';
+}
+
+// 2nd
+function solution(cards1, cards2, goal) {
+  for (let v of goal) {
+    if (cards1.includes(v)) {
+      if (cards1.shift() !== v) {
+        return 'No';
+      }
+    } else {
+      if (cards2.shift() !== v) {
+        return 'No';
+      }
+    }
+  }
+  return 'Yes';
+}
