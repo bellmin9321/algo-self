7980721427a49c2423e8e8e7c5a63eefa3029c26
JONGMIN KIM <bellmin9321@gmail.com>
2023-04-21 02:45:35 +0900

    ⭕️ Solved

---
+++ other/purple/7. 중간 숫자 찾기.js
@@ -0,0 +1,20 @@
+/*
+3개의 숫자 중에 2번째 크기의 숫자 위치를 찾는 함수를 작성해주세요.
+
+**예시**
+
+- ex) [0,1,2] ⇒ 1
+- ex) [5,1,2] ⇒ 2
+*/
+
+const foo = (arr) => {
+  arr.sort((a, b) => a - b);
+
+  return arr[1];
+}
+// 원본 배열 유지 풀이
+const bar = (arr) => {
+  const copied = [...arr].sort((a, b) => a - b);
+
+  return copied[1];
+}
