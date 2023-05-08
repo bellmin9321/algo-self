99dc14399cd5ff0aec270e54e230509d1649ab84
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-08 23:46:48 +0900

    ❌ NotSolved

---
+++ src/leetcode/2670. Find the Distinct Difference Array.js
@@ -0,0 +1,10 @@
+// my soltution: ❌ NotSolved
+var distinctDifferenceArray = function(nums) {
+  return nums.map((v, i) => {
+    if (nums[nums.length - 1 - v]) {
+      return (i + 1) - nums[nums.length - 1 - v];
+    }
+
+    return v;
+  })
+};
