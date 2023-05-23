d4b76dca24a2f36154003516c637188db1020339
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-24 00:10:18 +0900

    ⭕️ Solved: 2번째 풀이

--- src/programmers/주차 요금 계산.js
+++ src/programmers/주차 요금 계산.js
@@ -1,3 +1,32 @@
+// 2번째 풀이: ⭕️ Solved(30min) 23.05.15
+function solution(fees, records) {
+  const map = {};
+
+  records.forEach(record => {
+    const [time, number, type] = record.split(' ');
+    const [h, m] = time.split(':');
+    const totalMins = +h * 60 + +m;
+
+    if (map[number]) map[number].push(totalMins);
+    else map[number] = [totalMins];
+  });
+
+  const result = Object.entries(map)
+    .sort((a, b) => a[0] - b[0])
+    .map(([, arr]) => {
+      const [defaultTime, defaultFee, perTime, perFee] = fees;
+      if (arr.length % 2 === 1) {
+        arr.push(1439);
+      }
+      const sum = arr.reduce((a, c, i) => (a += i % 2 === 1 ? c : -c), 0);
+      if (sum <= defaultTime) return defaultFee;
+
+      return defaultFee + Math.ceil((sum - defaultTime) / perTime) * perFee;
+    });
+
+  return result;
+}
+
 // my solution
 function solution(fees, records) {
   const [time, fee, unitTime, unitFee] = fees;
@@ -24,4 +53,24 @@ function solution(fees, records) {
 
   arr.sort((a, b) => +a[0] - +b[0]);
   return arr.map(([_, sum]) => sum);
+}
+
+// best
+function solution(fees, records) {
+  const parkingTime = {};
+  records.forEach(r => {
+    let [time, id, type] = r.split(' ');
+    let [h, m] = time.split(':');
+    time = h * 1 * 60 + m * 1;
+    if (!parkingTime[id]) parkingTime[id] = 0;
+    if (type === 'IN') parkingTime[id] += 1439 - time;
+    if (type === 'OUT') parkingTime[id] -= 1439 - time;
+  });
+  const answer = [];
+  for (let [car, time] of Object.entries(parkingTime)) {
+    if (time <= fees[0]) time = fees[1];
+    else time = Math.ceil((time - fees[0]) / fees[2]) * fees[3] + fees[1];
+    answer.push([car, time]);
+  }
+  return answer.sort((a, b) => a[0] - b[0]).map(v => v[1]);
 }
