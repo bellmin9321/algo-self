2c676edc5dd240364693471047cbc39b613d3892
JONGMIN KIM <bellmin9321@gmail.com>
2023-05-30 01:32:49 +0900

    ⭕️ Solved: 2번째 풀이

--- src/programmers/비밀지도.js
+++ src/programmers/비밀지도.js
@@ -1,3 +1,19 @@
+// 2번째 푼 풀이: ⭕️ Solved(23.05.12)
+function solution(n, arr1, arr2) {
+  return arr1.map((v, i) => {
+    const binary1 = v.toString(2).padStart(n, 0);
+    const binary2 = arr2[i].toString(2).padStart(n, 0);
+    let ans = '';
+
+    for (let j = 0; j < binary1.length; j++) {
+      if (binary1[j] === '1' || binary2[j] === '1') ans += '#';
+      else ans += ' ';
+    }
+
+    return ans;
+  });
+}
+
 function solution(n, arr1, arr2) {
   const binaryArr1 = arr1.map(v => v.toString(2));
   const binaryArr2 = arr2.map(v => v.toString(2));
