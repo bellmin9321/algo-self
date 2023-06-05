2ad762c0264943af396f9a8fd8935ca1b5c098e7
JONGMIN KIM <bellmin9321@gmail.com>
2023-06-05 14:56:44 +0900

    ⭕️ Solved: 2번째 풀이

--- src/programmers/야근 지수.js
+++ src/programmers/야근 지수.js
@@ -1,4 +1,18 @@
-// my solution: 2시간 동안 품
+// 2번째 풀이: ⭕️ Solved(40min) -> 23.05.14
+function solution(n, works) {
+  if (n > works.reduce((a, b) => a + b)) return 0;
+
+  while (n > 0) {
+    const max = Math.max(...works);
+    works.forEach((v, i) => {
+      if (v === max && max > 0) works[i] -= n-- > 0 ? 1 : 0;
+    });
+  }
+
+  return works.reduce((a, b) => (a += b ** 2), 0);
+}
+
+// my solution: ❌ NotSolved -> 2시간 동안 품
 function solution(n, works) {
   if (sum(works) <= n) return 0;
 
@@ -20,6 +34,16 @@ function solution(n, works) {
 }
 
 const sum = arr => arr.reduce((a, b) => a + b);
+
+// New best
+const noOvertime = (no, works) => {
+  while (no > 0) {
+    works.sort((a, b) => b - a);
+    works[0] -= 1;
+    no--;
+  }
+  return works.map(a => a * a).reduce((a, b) => a + b);
+};
 
 // best -> 시간 초과 발생
 const solution = (n, works) => {
