5264cd5e01a7b5244231d8e262ec9aaf3d372bc5
JONGMIN KIM <bellmin9321@gmail.com>
2023-09-02 23:42:20 +0900

    ❌ NotSolved: 효율성 테스트 (2/4) 실패

---
+++ src/programmers/전화번호 목록.js
@@ -0,0 +1,18 @@
+// ❌ NotSolved: 효율성 테스트 (2/4) 실패
+function solution(phone_book) {
+  for (let i = 0; i <= phone_book.length - 1; i++) {
+      const target = phone_book[i];
+      
+      for (let j = 0; j <= phone_book.length - 1; j++) {
+          if (i === j) continue;
+
+          const prefixIndex = phone_book[j].indexOf(target);
+          
+          if (prefixIndex === 0) {
+              return false;
+          }
+      }
+  }
+  
+  return true;
+}
