18a82e28d5be784c8fabf76451cf386f0ced7a7f
bellmin <bellmin9321@gmail.com>
2024-03-09 18:16:44 +0900

    [add] best solution in 네트워크(level3)

--- src/programmers/네트워크.js
+++ src/programmers/네트워크.js
@@ -52,6 +52,33 @@ function solution(n, computers) {
 }
 
 // best
+let arr;
+let visitArr;
+
+function solution(n, computers) {
+  let ctr = 0;
+  arr = computers;
+  visitArr = new Array(n).fill(false);
+
+  for (let i in arr) {
+    ctr += dfs(i);
+  }
+
+  return ctr;
+}
+
+function dfs(i) {
+  if (visitArr[i] == true) return 0;
+  else visitArr[i] = true;
+
+  for (let j in arr[i]) {
+    if (arr[i][j] == 1) dfs(j);
+  }
+
+  return 1;
+}
+
+// 1st
 function solution(n, computers) {
   let answer = 0;
   const isVisited = [];
