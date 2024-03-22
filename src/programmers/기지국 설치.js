36137fb64efbbcbde62c03cc519bdfd07621c3dc
bellmin <bellmin9321@gmail.com>
2024-03-22 16:33:40 +0900

    ❌ NotSolved

---
+++ src/programmers/기지국 설치.js
@@ -0,0 +1,67 @@
+// ❌ NotSolved(1h) -> 기본 테스트 (16/21) 통과, 시간 초과 및 해결책 x
+// my solution1
+function solution(n, stations, w) {
+  let ans = 0;
+  let totalStations = Array(n).fill().map((_, i) => i + 1);
+  
+  for (let i = 0; i < stations.length; i++) {
+      const station = stations[i]
+      
+      totalStations = totalStations.filter((v) => {
+          return !(station - w <= v && v <= station + w);
+      })
+  }
+  
+  let count = 0;
+  
+  for (let i = 0; i < totalStations.length - 1; i++) {
+      let j = i + 1;
+      const width = (w * 2) + 1;
+      
+      const before = totalStations[i];
+      const after = totalStations[j];
+      
+      if (after - before === 1) {
+          count++;
+          
+          if (count === width) {
+              ans++;
+              count = 0;
+              
+              if (j === totalStations.length - 1) {
+                  break;
+              }
+          }
+          
+          if (j === totalStations.length - 1) {
+              ans++;
+              break;
+          }
+          
+          continue;
+      }
+      
+      ans++;
+  }
+  
+  return ans;
+}
+
+// best
+function solution(n, stations, w) {
+  let ans = 0;
+  let startIndex = 0;
+  const width = (w * 2) + 1;
+  
+  for (let i = 0; i < stations.length; i++) {
+      const station = stations[i];
+      let endIndex = station - w - 1;
+      
+      ans += Math.ceil((endIndex - startIndex) / width);
+      startIndex = station + w;
+  }
+  
+  ans += Math.ceil((n - startIndex) / width);
+  
+  return ans;
+}
